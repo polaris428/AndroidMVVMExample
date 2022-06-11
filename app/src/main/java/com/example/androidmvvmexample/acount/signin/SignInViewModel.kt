b1bc2e5fac4d5b1576ffd93bb76022ffd7a33ea4
polaris <61411085+polaris428@users.noreply.github.com>
2022-06-12 01:34:06 +0900

    로그인 데이터 저장

--- app/src/main/java/com/example/androidmvvmexample/acount/signin/SignInViewModel.kt
+++ app/src/main/java/com/example/androidmvvmexample/acount/signin/SignInViewModel.kt
@@ -3,27 +3,42 @@ package com.example.androidmvvmexample.acount.signin
 import android.app.Application
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.*
 
 class SignInViewModel(application: Application) : AndroidViewModel(application) {
+
     val email = MutableLiveData<String>()
     var paw = MutableLiveData<String>()
     private val firebaseRepository = FirebaseAccountRepository(application)
-    fun onSignIn(){
-        var accountDate:AccountDate = firebaseRepository.getDate()
-        if(accountDate.email != "")   firebaseRepository.singIn(accountDate.email,accountDate.password
-        )
+    init {
+        onSignIn()
+
+    }
+
+    private fun onSignIn(){
+        val accountDate:AccountDate = firebaseRepository.getDate()
+        Log.e(accountDate.email,accountDate.password)
+        if(accountDate.email != "")   firebaseRepository.singIn(accountDate.email,accountDate.password)
+
+
+
 
     }
     fun onSignInButton(view: View) {
-        firebaseRepository.singIn(email.toString(),paw.toString())
+        firebaseRepository.singIn(email.value.toString(),paw.value.toString())
         Log.e("이메일", email.value.toString())
-        firebaseRepository.setDate(email.value.toString(),paw.value.toString())
+        savaSignData()
 
     }
     fun onSignUpButton(view: View){
         firebaseRepository.sigUp(email.value.toString(),paw.value.toString())
-        firebaseRepository.setDate(email.value.toString(),paw.value.toString())
+        Log.e(email.value,paw.value.toString())
+        savaSignData()
     }
+
+    fun savaSignData() = firebaseRepository.setDate(email.value.toString(),paw.value.toString())
+
+
 
 }
